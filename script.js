4b344b41d85dfef9f4e1be836349c53afe31b707
AasmaNoor123 <aasmanoor511@gmail.com>
2024-12-18 17:42:34 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let btn = document.getElementById("btn");
+btn.onclick=()=> {
+    let inputText = document.getElementById('inputText').value;
+    let resultDiv = document.getElementById('result');
+    let errorDiv = document.getElementById('error');
+    resultDiv.textContent = '';
+    errorDiv.textContent = '';
+
+    if (!inputText) {
+      errorDiv.textContent = 'Please enter some text.';
+      return;
+    }
+
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    let count = 0;
+
+    for (let i = 0; i < inputText.length; i++) {//noor
+      if (vowels.includes(inputText[i])) {//
+        count++;
+      }
+    }
+
+    resultDiv.textContent = 'Number of vowels: ' + count;
+  }
